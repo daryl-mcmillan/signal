36feeb0ac6193785da6c6031cc7b14b37351913f
Daryl McMillan <darylm+github@gmail.com>
2016-10-01 18:27:01 -0400

    serial write

--- main.cc
+++ main.cc
@@ -9,7 +9,28 @@ typedef uint8_t byte;
 #define SERIAL_BAUD 9600
 #define SERIAL_PRESCALE ((F_CPU / ( SERIAL_BAUD * 16UL )) - 1)
 
+void serialSetup( int baud ) {
+  int prescale = (F_CPU / ( baud * 16UL )) - 1;
+  UCSR0C = ( 0 << UMSEL00 ) | ( 0 << UPM00 ) | ( 0 << USBS0 )
+        | ( 0b11 << UCSZ00 ) | ( 0 << UCPOL0 );
+  UBRR0 = prescale;
+  UCSR0B = ( 1 << RXEN0 ) | ( 1 << TXEN0 );
+}
+
+void serialWrite( const char* str ) {
+  while( *str ) {
+    while( !( UCSR0A & ( 1 << UDRE0 ) ) ) { }
+    UDR0 = *str;
+    str++;
+  }
+}
+
 int main(void) {
+
+  serialSetup( 19200 );
+  for( ;; ) {
+    serialWrite( "this is a test\n" );
+  }
 
   DDRD = 0xFF;
 //  for( ;; ) {
@@ -17,11 +38,6 @@ int main(void) {
 //    PORTD = 0x00;
 //  }
   signal(0b0101010100110011);
-
-  UCSR0C = ( 0 << UMSEL00 ) | ( 0 << UPM00 ) | ( 0 << USBS0 )
-	| ( 0b11 << UCSZ00 ) | ( 0 << UCPOL0 );
-  UBRR0 = SERIAL_PRESCALE;
-  UCSR0B = ( 1 << RXEN0 ) | ( 1 << TXEN0 );
 
   DDRB = 0b100000;
   for( ;; ) {
