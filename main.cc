d28aa603d446ae9b3336e5cb0bb0702057796a16
Daryl McMillan <darylm+github@gmail.com>
2016-10-01 21:16:25 -0400

    serial blocking write, interrupt read

--- main.cc
+++ main.cc
@@ -14,22 +14,45 @@ void serialSetup( int baud ) {
   UCSR0C = ( 0 << UMSEL00 ) | ( 0 << UPM00 ) | ( 0 << USBS0 )
         | ( 0b11 << UCSZ00 ) | ( 0 << UCPOL0 );
   UBRR0 = prescale;
-  UCSR0B = ( 1 << RXEN0 ) | ( 1 << TXEN0 );
+  UCSR0B = ( 1 << RXEN0 ) | ( 1 << TXEN0 ) | ( 1 << RXCIE0 );
+  sei();
+}
+
+void serialWrite( const char c ) {
+  while( !( UCSR0A & ( 1 << UDRE0 ) ) ) { }
+  UDR0 = c;
 }
 
 void serialWrite( const char* str ) {
   while( *str ) {
-    while( !( UCSR0A & ( 1 << UDRE0 ) ) ) { }
-    UDR0 = *str;
+    serialWrite( *str );
     str++;
   }
+}
+
+const char * HEX = "0123456789ABCDEF";
+
+void serialWrite( unsigned int number ) {
+  serialWrite( "0x" );
+  for( int shift = 28; shift >= 0; shift -=4 ) {
+    serialWrite( HEX[(number >> shift) & 0xF] );
+  }
+}
+
+volatile unsigned int counter = 17;
+
+ISR(USART_RX_vect) {
+  byte val = UDR0;
+  counter ++;
 }
 
 int main(void) {
 
   serialSetup( 19200 );
   for( ;; ) {
-    serialWrite( "this is a test\n" );
+    serialWrite( "this is a test " );
+    serialWrite( counter );
+    serialWrite( "\n" );
   }
 
   DDRD = 0xFF;
